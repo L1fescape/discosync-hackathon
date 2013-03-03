4ac0e2d795da1d6a77e1d961a3e713f0bda27aa2
Tommy Goode <tommy@tgoode.com>
2013-03-02 19:24:54 -0800

    Adding a Mac app for streaming from the computer.

---
+++ Mac/classes/DSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSAppDelegate.h
+//  DiscoSyncStreamer
+//
+//  Created by Tommy Goode on 3/2/13.
+//  Copyright (c) 2013 Feathr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Mac/classes/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DiscoSyncStreamer
+//
+//  Created by Tommy Goode on 3/2/13.
+//  Copyright (c) 2013 Feathr. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
