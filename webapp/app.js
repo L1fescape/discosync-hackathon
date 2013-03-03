825d02f30caba06aeb649bb3fa0f2c45040ee9a1
Andrew Kennedy <akennedy2292@gmail.com>
2013-03-03 01:08:32 -0800

    removed useless stuff

--- webapp/app.js
+++ webapp/app.js
@@ -9,8 +9,6 @@ var port = 1337;
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
 	, fs = require('fs')
