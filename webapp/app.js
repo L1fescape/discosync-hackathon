401d29b5182d39a81b23b7b9fe5bfca061bf12b0
Andrew Kennedy <akennedy2292@gmail.com>
2013-03-03 13:25:17 -0800

    fixing app

--- webapp/app.js
+++ webapp/app.js
@@ -48,37 +48,6 @@ app.configure('development', function(){
 // Index
 app.get('/', routes.index);
 // Sample song with chunked responses
-app.get('/polynation.mp3', function(request, response) {
-	var filename = "public/songs/polynation.mp3";
-	fs.readFile(filename, "binary", function(err, file) {
-		var header = {};
-		if (typeof request.headers.range !== 'undefined') {
-			var range = request.headers.range; 
-			var parts = range.replace(/bytes=/, "").split("-"); 
-			var partialstart = parts[0]; 
-			var partialend = parts[1]; 
- 
-			var total = file.length; 
- 
-			var start = parseInt(partialstart, 10); 
-			var end = partialend ? parseInt(partialend, 10) : total-1;
- 
-			header["Content-Range"] = "bytes " + start + "-" + end + "/" + (total);
-			header["Accept-Ranges"] = "bytes";
-			header["Content-Length"]= (end-start)+1;
-			header['Transfer-Encoding'] = 'chunked';
-			header["Connection"] = "close";
- 
-			response.writeHead(206, header); 
-			response.write(file.slice(start, end)+'0', "binary");
-		}
-		else{
-			response.writeHead(200, header);
-			response.write(file, "binary");
-		}
-		response.end();
-	});
-});
 
 
 /** 
