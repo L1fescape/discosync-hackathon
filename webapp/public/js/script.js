1bc4675f432e2c82fc47781ac9379460cc0ef968
williamnewton <williamnewbton@gmail.com>
2013-03-02 23:55:58 -0500

    Have DJ mode layout ready- need to add DJ interface UI

--- webapp/public/js/script.js
+++ webapp/public/js/script.js
@@ -10,4 +10,19 @@ $(document).ready(function(){
 		$(".app_room").animate({opacity : 0}, "fast" );
 		$(".app_roomlist").animate({opacity : 1}, "slow" );
 	});
+	$(".letsDJ").click(function(){
+		$(".frame").animate({opacity : 0}, "slow" );
+		$(".app_main").animate({width : "1000px"}, "slow" );
+		$(".letsDJ").animate({opacity : 1}, "slow");
+		$(".closeDJ").animate({opacity : 0.5}, "slow");
+		$(".interfaceDJ").animate({opacity : 1, top : "100px"}, 1000);
+	});
+	$(".closeDJ").click(function(){
+		$(".app_main").animate({width : "600px"}, "slow" );
+		$(".letsDJ").animate({opacity : 0.5}, "slow");
+		$(".frame").animate({opacity : 1}, "slow" );
+		$(".closeDJ").animate({opacity : 0}, "slow");
+		$(".interfaceDJ").animate({opacity : 0}, "fast");
+		$(".interfaceDJ").animate({top : "1000px"}, "fast");
+	});
 });
