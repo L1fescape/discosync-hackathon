2f772193146646c026cd9f2b7ebd8fbadafbcc35
Andrew Kennedy <akennedy2292@gmail.com>
2013-03-03 16:40:08 -0800

    this doesn't work, saving for working on later

--- webapp/app.js
+++ webapp/app.js
@@ -58,12 +58,55 @@ var decoder = new ogg.Decoder();
 var encoder = new ogg.Encoder();
 var file = __dirname + '/AmorFati.ogg'
 var disco = new Firebase('https://disco-sync.firebaseio.com/');
-// pipe the ogg file to the Decoder
-fs.createReadStream(file);
-fs.on('data', function(packet) {
-	console.log("sending packet");
-	disco.child('stream').push(outStream);
+var out = path.resolve(path.dirname(file), 'copy of ' + path.basename(file));
+var getKeys = function(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
+}
+decoder.on('stream', function (stream) {
+
+  // we match the serial numbers, though we could also get random ones instead
+  var outStream = encoder.stream(stream.serialno);
+
+  // for each "page" event, force the output stream to flush a page of its own.
+  // the first time this is emitted there won't have been any "packets" queued
+  // yet, but it's nothing to worry about...
+  stream.on('page', function (page, done) {
+    outStream.flush(done);
+  });
+
+  // for each "packet" event, send the packet to the output stream untouched
+  stream.on('packet', function (packet, done) {
+    outStream.packetin(packet, done);
+  });
+	
+  // at the end of each stream, force one last page flush, for any remaining
+  // packets in the stream. this ensures the "end" event gets fired properly.
+  stream.on('eos', function () {
+    outStream.flush(function (err) {
+      if (err) throw err;
+    });
+  });
 });
+fs.createReadStream(file).pipe(decoder);
+// the OGG file output from the Encoder instance gets piped to the output file
+encoder.pipe(fs.createWriteStream(out));
+encoder.on('_page', function() {
+	console.log("asdfasdf");
+});
+encoder.on('end', function () {
+  console.error('created copy of %j as %j', file, out);
+});
+
+
+/*var readStream = fs.createReadStream(file);
+readStream.on('data', function(packet) {
+	console.log("sending packet");
+	disco.child('stream').push(packet, function(resp) { console.log(resp); });
+});*/
 
 /** 
  * Initialize listener
