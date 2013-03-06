7417b0b83ce3de552cce26ed16126d099e6f02c3
Andrew Kennedy <akennedy2292@gmail.com>
2013-03-05 16:57:40 -0800

    people listening -> listening

--- webapp/js/templates.js
+++ webapp/js/templates.js
@@ -3,14 +3,14 @@ var templates = {
 		return '<div class="roomlist_item" key="{{ name }}"> \
 			      <div class="djname">{{ val.name }}</div> \
 					  <div class="genre">{{ val.genre }}</div> \
-						<div class="listeners">{{ val.listeners }} people listening</div> \
+						<div class="listeners">{{ val.listeners }} listening</div> \
 						<div class="carrot"></div> \
 						</div>';
 	},
 	itemRoom_inner : function() {
 		return '<div class="djname">{{ name }}</div> \
 					  <div class="genre">{{ genre }}</div> \
-						<div class="listeners">{{ listeners }} people listening</div> \
+						<div class="listeners">{{ listeners }} listening</div> \
 						<div class="carrot"></div>';
 	}
 };
